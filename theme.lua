7437caadcaea90c5809f588a21feebec80465b57
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2025-04-07 12:10:18 +0700

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -1,23 +1,26 @@
+-- TAKEN FROM RealzzHub FULL CREDIT: https://raw.githubusercontent.com/RealZzHub/MainV2/main/Misc/themes.lua
+
+
 return { 
     {
         Name = "Default",
         Background = "",
         Logo = "",
-        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
-        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+        Color1 = Color3.fromRGB(38, 229, 255),
+        Color2 = Color3.fromRGB(20, 255, 20)
     },
     {
         Name = "Valentines Day",
-        Background = "rbxassetid://78505844559912",
-        Logo = "rbxassetid://86024897947944",
-        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
-        Color2 = Color3.fromRGB(169, 169, 169)   -- Màu trắng
+        Background = "rbxassetid://12471127452",
+        Logo = "rbxassetid://12471122537",
+        Color1 = Color3.fromRGB(233, 149, 211),
+        Color2 = Color3.fromRGB(171, 96, 251)
     },
     {
         Name = "Orange Red",
-        Background = "rbxassetid://78505844559912",
-        Logo = "rbxassetid://86024897947944",
-        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
-        Color2 = Color3.fromRGB(169, 169, 169)   -- Màu trắng
+        Background = "rbxassetid://8744740550",
+        Logo = "rbxassetid://8744739134",
+        Color1 = Color3.fromRGB(254, 0, 0),
+        Color2 = Color3.fromRGB(255, 182, 0)
     },
 }
