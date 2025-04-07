723889f2fae578e60b189c9118e34f8da9cef7dc
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2025-04-07 12:15:58 +0700

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -8,14 +8,14 @@ return {
     },
     {
         Name = "Valentines Day",
-        Background = "rbxassetid://12471127452",
+        Background = "rbxassetid://86024897947944",
         Logo = "rbxassetid://86024897947944",
         Color1 = Color3.fromRGB(0, 0, 0),  -- Màu đen
         Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
     },
     {
         Name = "Orange Red",
-        Background = "rbxassetid://8744740550",
+        Background = "rbxassetid://86024897947944",
         Logo = "rbxassetid://86024897947944",
         Color1 = Color3.fromRGB(0, 0, 0),  -- Màu đen
         Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
