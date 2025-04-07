9d361490a68304012501ee18ad4446b6ff5aef07
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2025-04-07 12:56:42 +0700

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1841,7 +1841,7 @@ end)
                 ConfigDropdown:Set(configs[1])
             end
         end)
-        CustomCredits = CustomCredits or tostring(game:HttpGet("https://raw.githubusercontent.com/RealZzHub/MainV2/main/Misc/Credits.txt"))
+        CustomCredits = CustomCredits or tostring(game:HttpGet("https://raw.githubusercontent.com/mkhoidzvl13/dead/refs/heads/main/Credits.txt"))
         local Credits = string.split((CustomCredits), ",")
         ConfigTab:NewDropdown("Credits", Credits, function(v)
             return
