3b3f6304ecc60f8c0fee885c43b15ddf3885c463
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2025-04-07 11:58:03 +0700

    Update theme.lua

---
+++ theme.lua
@@ -0,0 +1,23 @@
+return { 
+    {
+        Name = "Default",
+        Background = "",
+        Logo = "",
+        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
+        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+    },
+    {
+        Name = "Valentines Day",
+        Background = "rbxassetid://78505844559912",
+        Logo = "rbxassetid://107961805648748",
+        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
+        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+    },
+    {
+        Name = "Orange Red",
+        Background = "rbxassetid://78505844559912",
+        Logo = "rbxassetid://8744739134",
+        Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
+        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+    },
+}
