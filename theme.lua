eff82c12cb7c71f0876b99ef2eb694fe9eb89c1a
Grayx <112261602+mkhoidzvl13@users.noreply.github.com>
2025-04-07 12:09:35 +0700

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -11,13 +11,13 @@ return {
         Background = "rbxassetid://78505844559912",
         Logo = "rbxassetid://86024897947944",
         Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
-        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+        Color2 = Color3.fromRGB(169, 169, 169)   -- Màu trắng
     },
     {
         Name = "Orange Red",
         Background = "rbxassetid://78505844559912",
         Logo = "rbxassetid://86024897947944",
         Color1 = Color3.fromRGB(169, 169, 169),  -- Màu xám
-        Color2 = Color3.fromRGB(255, 255, 255)   -- Màu trắng
+        Color2 = Color3.fromRGB(169, 169, 169)   -- Màu trắng
     },
 }
